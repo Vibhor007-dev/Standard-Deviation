8c39eae60c4af406115dbcde64739e6fcf597e19
Vibhor Singh <78255056+Vibhor007-dev@users.noreply.github.com>
2022-01-14 13:14:30 +0530

    Add files via upload

---
+++ standardDeviation.py
@@ -0,0 +1,34 @@
+import csv
+import math
+
+with open("data.csv",newline='') as f:
+    reader=csv.reader(f)
+    new_data=list(reader)
+
+data=new_data[0]
+
+def mean(data):
+    n=len(data)
+    total=0
+    
+    for i in data:
+        total+=int(i)
+
+    mean=total/n
+    return mean
+
+squared_list=[]
+
+for number in data:
+    a=int(number) - mean(data)
+    a=a**2
+    squared_list.append(a)
+
+sum=0
+for i in squared_list:
+    sum=sum+i
+
+result = sum/(len(data)-1)
+
+sd=math.sqrt(result)
+print(sd)
